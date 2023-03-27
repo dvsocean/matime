84fb275875cc95d90c2c248c2080a24b812b6c47
Daniel Ocean <danielocean@Daniels-MacBook-Pro.local>
2023-03-26 18:40:58 -0700

    adding welcome routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'home', to: 'home#index'
-  # root 'landing#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get 'welcome_page/welcome'
+  # get 'home', to: 'home#index'
+  # # root 'landing#index'
+  # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "home#index"
+  # # Defines the root path route ("/")
+  # root "home#index"
+
+  get 'welcome_page/welcome'
+  root 'welcome_page#welcome'
 end
